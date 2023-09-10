43bd6881b5ade1be47ec6b73bda103dfd4f7b694
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 09:54:29 +0200

    Code Editing AuthorController.cs

--- BookStore_App/Controllers/AuthorController.cs
+++ BookStore_App/Controllers/AuthorController.cs
@@ -7,127 +7,127 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore_App.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class AuthorController : ControllerBase
-	{
-		private readonly IAuthorService _authorService;
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AuthorController : ControllerBase
+     {
+	 private readonly IAuthorService _authorService;
 
-        public AuthorController(IAuthorService authorService)
-        {
-			_authorService = authorService;
-        }
+         public AuthorController(IAuthorService authorService)
+           {
+	      _authorService = authorService;
+           }
 
-		[HttpGet("{id}")]
+	   [HttpGet("{id}")]
 
-		public async Task<ActionResult<AuthorDto>> GetAuthorByIdAsync(int? id)
-		{
-			try
+	    public async Task<ActionResult<AuthorDto>> GetAuthorByIdAsync(int? id)
+	    {
+		try
+	         {
+		    if (id == 0)
 			{
-				if (id == 0)
-				{
-					return BadRequest("Id can not be null.");
-				}
-
-				if (id <= 0)
-				{
-					return BadRequest("Id can not be a negative number");
-				}
-
-				AuthorDto authorDto = await _authorService.GetAuthorByIdAsync(id);
-
-				return Ok(authorDto);
+			   return BadRequest("Id can not be null.");
 			}
-			catch (Exception)
+
+		    if (id <= 0)
 			{
-				return StatusCode(StatusCodes.Status500InternalServerError, "Please contact the support team.");
+			    return BadRequest("Id can not be a negative number");
 			}
+
+		        AuthorDto authorDto = await _authorService.GetAuthorByIdAsync(id);
+
+			return Ok(authorDto);
+		  }
+		catch (Exception)
+		 {
+		    return StatusCode(StatusCodes.Status500InternalServerError, "Please contact the support team.");
+		 }
+	     }
+
+	    [HttpGet]
+
+	    public async Task<ActionResult<List<AuthorDto>>> GetAllAuthorsAsync()
+	      {
+		 try
+		  {
+		      return Ok(await _authorService.GetAllAuthorsAsync());
+		  }
+		 catch (Exception)
+		  {
+		      return StatusCode(StatusCodes.Status500InternalServerError, "Please contact the support team.");
+		  }
 		}
 
-		[HttpGet]
+	      [HttpPost]
 
-		public async Task<ActionResult<List<AuthorDto>>> GetAllAuthorsAsync()
+	      public async Task<ActionResult> CreateAuthorAsync([FromBody]CreateAuthorDto author)
 		{
-			try
-			{
-				return Ok(await _authorService.GetAllAuthorsAsync());
-			}
-			catch (Exception)
-			{
-				return StatusCode(StatusCodes.Status500InternalServerError, "Please contact the support team.");
-			}
+		  try
+		   {
+		     if (author == null || author.FirstName == null || author.LastName == null || author.City == null || author.State == null)
+		     {
+			return BadRequest("Invalid input");
+		     }
+
+			await _authorService.CreateAuthorAsync(author);
+			return StatusCode(StatusCodes.Status201Created, "Author added");
+		    }
+		   catch (Exception)
+		    {
+			return StatusCode(StatusCodes.Status500InternalServerError, "Please contact the support team.");
+		    }
 		}
 
-		[HttpPost]
+	      [HttpDelete("{id}")]
 
-		public async Task<ActionResult> CreateAuthorAsync([FromBody]CreateAuthorDto author)
+	      public async Task<ActionResult> DeleteAuthorAsync(int id)
 		{
-			try
+		   try
+		    {
+			if (id == 0)
 			{
-				if (author == null || author.FirstName == null || author.LastName == null || author.City == null || author.State == null)
-				{
-					return BadRequest("Invalid input");
-				}
-
-				await _authorService.CreateAuthorAsync(author);
-				return StatusCode(StatusCodes.Status201Created, "Author added");
+			   return BadRequest("Id can not be null");
 			}
-			catch (Exception)
+
+			if (id <= 0)
 			{
-				return StatusCode(StatusCodes.Status500InternalServerError, "Please contact the support team.");
+			    return BadRequest("Id can not be a negative number");
 			}
-		}
 
-		[HttpDelete("{id}")]
+			await _authorService.DeleteAuthorAsync(id);
 
-		public async Task<ActionResult> DeleteAuthorAsync(int id)
-		{
-			try
-			{
-				if (id == 0)
-				{
-					return BadRequest("Id can not be null");
-				}
-
-				if (id <= 0)
-				{
-					return BadRequest("Id can not be a negative number");
-				}
-
-				await _authorService.DeleteAuthorAsync(id);
-
-				return Ok();
-			}
-			catch (Exception)
-			{
-				return StatusCode(StatusCodes.Status500InternalServerError, "Please contact the support team.");
-			}
-		}
+			return Ok();
+		      }
+		     catch (Exception)
+		      {
+			return StatusCode(StatusCodes.Status500InternalServerError, "Please contact the support team.");
+		      }
+		  }
 
 		[HttpPatch("{id}")]
 
 		public async Task<ActionResult> UpdateAuthorAsync([FromBody]CreateAuthorDto createAuthorDto, int id)
 		{
-			try
+		    try
+		     {
+			if (id == 0)
 			{
-				if (id == 0)
-				{
-					return BadRequest("Id can not be null");
-				}
-
-				if (id <= 0)
-				{
-					return BadRequest("Id can not be a negative number");
-				}
-
-				await _authorService.UpdateAuthorAsync(createAuthorDto, id);
-
-				return Ok();
+			   return BadRequest("Id can not be null");
 			}
-			catch (Exception)
+
+			if (id <= 0)
 			{
-				return StatusCode(StatusCodes.Status500InternalServerError, "Please contact the support team.");
+			    return BadRequest("Id can not be a negative number");
 			}
-		}
+
+			await _authorService.UpdateAuthorAsync(createAuthorDto, id);
+
+			return Ok();
+		      }
+		     catch (Exception)
+		      {
+			  return StatusCode(StatusCodes.Status500InternalServerError, "Please contact the support team.");
+		      }
+	          }
 	}
 }
