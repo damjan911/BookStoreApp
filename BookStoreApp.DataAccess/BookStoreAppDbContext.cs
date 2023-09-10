2c2b0627b55f53e70630337efdc4fea54eee824a
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 11:25:28 +0200

    Code Editing BookStoreAppDbContext.cs

--- BookStoreApp.DataAccess/BookStoreAppDbContext.cs
+++ BookStoreApp.DataAccess/BookStoreAppDbContext.cs
@@ -9,182 +9,182 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.DataAccess
 {
-	public class BookStoreAppDbContext : DbContext
-	{
-		public BookStoreAppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
-		{
+    public class BookStoreAppDbContext : DbContext
+    {
+	 public BookStoreAppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+	 {
 
-		}
+	 }
 
-		public DbSet<Author> Authors { get; set; }
+	  public DbSet<Author> Authors { get; set; }
 
-		public DbSet<Book> Books { get; set; }
+	  public DbSet<Book> Books { get; set; }
 
-		public DbSet<Customer> Customers { get; set; }
+	  public DbSet<Customer> Customers { get; set; }
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			base.OnModelCreating(modelBuilder);
+	  protected override void OnModelCreating(ModelBuilder modelBuilder)
+	  {
+		base.OnModelCreating(modelBuilder);
 
-			modelBuilder.Entity<Author>()
-			.Property(x => x.FirstName)
-			.HasMaxLength(50);
+		modelBuilder.Entity<Author>()
+		.Property(x => x.FirstName)
+		.HasMaxLength(50);
 
-			modelBuilder.Entity<Author>()
-			.Property(x => x.LastName)
-			.HasMaxLength(50);
+	        modelBuilder.Entity<Author>()
+		.Property(x => x.LastName)
+		.HasMaxLength(50);
 
-			modelBuilder.Entity<Author>()
-			.Property(x => x.City)
-			.HasMaxLength(50);
+		modelBuilder.Entity<Author>()
+		.Property(x => x.City)
+		.HasMaxLength(50);
 
-			modelBuilder.Entity<Author>()
-			.Property(x => x.State)
-			.HasMaxLength(50);
+		modelBuilder.Entity<Author>()
+		.Property(x => x.State)
+		.HasMaxLength(50);
 
 
-			modelBuilder.Entity<Book>()
-			.Property(x => x.Title)
-			.HasMaxLength(50);
+		 modelBuilder.Entity<Book>()
+		 .Property(x => x.Title)
+		 .HasMaxLength(50);
 
-			modelBuilder.Entity<Book>()
-			.Property(x => x.ISBN)
-			.HasMaxLength(50);
+		 modelBuilder.Entity<Book>()
+		 .Property(x => x.ISBN)
+		 .HasMaxLength(50);
 
-			modelBuilder.Entity<Book>()
-			.Property(x => x.Genre)
-			.IsRequired();
+		 modelBuilder.Entity<Book>()
+		 .Property(x => x.Genre)
+		 .IsRequired();
 
-			modelBuilder.Entity<Customer>()
-			.Property(x => x.FirstName)
-			.HasMaxLength(50);
+		 modelBuilder.Entity<Customer>()
+		 .Property(x => x.FirstName)
+		 .HasMaxLength(50);
 
-			modelBuilder.Entity<Customer>()
-			.Property(x => x.LastName)
-			.HasMaxLength(50);
+		 modelBuilder.Entity<Customer>()
+		 .Property(x => x.LastName)
+		 .HasMaxLength(50);
 
-			modelBuilder.Entity<Customer>()
-			.Property(x => x.Address)
-			.IsRequired();
+		 modelBuilder.Entity<Customer>()
+		 .Property(x => x.Address)
+		 .IsRequired();
 
-			modelBuilder.Entity<Book>()
-			.HasMany(x => x.BookCustomers)
-			.WithOne(x => x.Book)
-			.HasForeignKey(x => x.BookId);
+		 modelBuilder.Entity<Book>()
+		 .HasMany(x => x.BookCustomers)
+		 .WithOne(x => x.Book)
+		 .HasForeignKey(x => x.BookId);
 
-			modelBuilder.Entity<Customer>()
-			.HasMany(x => x.BookCustomers)
-			.WithOne(x => x.Customer)
-			.HasForeignKey(x => x.BookId);
+	         modelBuilder.Entity<Customer>()
+		 .HasMany(x => x.BookCustomers)
+		 .WithOne(x => x.Customer)
+		 .HasForeignKey(x => x.BookId);
 
-			modelBuilder.Entity<Author>()
-			.HasMany(x => x.Books)
-			.WithOne(x => x.Author)
-			.HasForeignKey(x => x.AuthorId);
+		  modelBuilder.Entity<Author>()
+		  .HasMany(x => x.Books)
+		  .WithOne(x => x.Author)
+		  .HasForeignKey(x => x.AuthorId);
 
-			modelBuilder.Entity<Author>().HasData(new Author
-			{
-				Id = 1,
-				FirstName = "Abraham",
-				LastName = "Bennet",
-				City = "Berkeley",
-				State = "CA"
-			});
+		  modelBuilder.Entity<Author>().HasData(new Author
+		  {
+			Id = 1,
+			FirstName = "Abraham",
+			LastName = "Bennet",
+			City = "Berkeley",
+			State = "CA"
+		  });
 
-			modelBuilder.Entity<Author>().HasData(new Author
-			{
-				Id = 2,
-				FirstName = "Johnson",
-				LastName = "White",
-				City = "Menlo Park",
-				State = "CA"
-			});
+		   modelBuilder.Entity<Author>().HasData(new Author
+		   {
+			 Id = 2,
+			 FirstName = "Johnson",
+			 LastName = "White",
+			 City = "Menlo Park",
+			 State = "CA"
+		    });
 
-			modelBuilder.Entity<Author>().HasData(new Author
-			{
-				Id = 3,
-				FirstName = "Charlene",
-				LastName = "Locksley",
-				City = "San Francisco",
-				State = "CA"
-			});
+		   modelBuilder.Entity<Author>().HasData(new Author
+		   {
+			  Id = 3,
+			  FirstName = "Charlene",
+			  LastName = "Locksley",
+			  City = "San Francisco",
+			  State = "CA"
+		    });
 
-			modelBuilder.Entity<Author>().HasData(new Author
-			{
-				Id = 4,
-				FirstName = "Dean",
-				LastName = "Straight",
-				City = "Oakland",
-				State = "CA"
+		    modelBuilder.Entity<Author>().HasData(new Author
+		    {
+			   Id = 4,
+			   FirstName = "Dean",
+			   LastName = "Straight",
+			   City = "Oakland",
+			   State = "CA"
+		     });
+
+		     modelBuilder.Entity<Book>().HasData(new Book
+		     {
+			   Id = 1,
+			   Title = "The Mystery of Shadows",
+			   ISBN = 978 - 123456789,
+			   Genre = Domain.Enums.Genre.Mystery,
+			   AuthorId = 3
+		     });
+
+		     modelBuilder.Entity<Book>().HasData(new Book
+		     {
+			    Id = 2,
+			    Title = "The Fantasy Realm",
+			    ISBN = 978 - 987654321,
+			    Genre = Domain.Enums.Genre.Fantasy,
+			    AuthorId = 2
+		     });
+
+		      modelBuilder.Entity<Book>().HasData(new Book
+		      {
+			     Id = 3,
+			     Title = "Thrills and Chills",
+			     ISBN = 978 - 543210987,
+			     Genre = Domain.Enums.Genre.Thriller,
+			     AuthorId = 4
+		       });
+
+		       modelBuilder.Entity<Book>().HasData(new Book
+		       {
+			      Id = 4,
+			      Title = "Enchanted Kingdom",
+			      ISBN = 978 - 567890123,
+			      Genre = Domain.Enums.Genre.Fantasy,
+			      AuthorId = 1
 			});
 
 			modelBuilder.Entity<Book>().HasData(new Book
 			{
-				Id = 1,
-				Title = "The Mystery of Shadows",
-				ISBN = 978 - 123456789,
-				Genre = Domain.Enums.Genre.Mystery,
-				AuthorId = 3
-			});
-
-			modelBuilder.Entity<Book>().HasData(new Book
-			{
-				Id = 2,
-				Title = "The Fantasy Realm",
-				ISBN = 978 - 987654321,
-				Genre = Domain.Enums.Genre.Fantasy,
-				AuthorId = 2
-			});
-
-			modelBuilder.Entity<Book>().HasData(new Book
-			{
-				Id = 3,
-				Title = "Thrills and Chills",
-				ISBN = 978 - 543210987,
-				Genre = Domain.Enums.Genre.Thriller,
-				AuthorId = 4
-			});
-
-			modelBuilder.Entity<Book>().HasData(new Book
-			{
-				Id = 4,
-				Title = "Enchanted Kingdom",
-				ISBN = 978 - 567890123,
-				Genre = Domain.Enums.Genre.Fantasy,
-				AuthorId = 1
-			});
-
-			modelBuilder.Entity<Book>().HasData(new Book
-			{
-				Id = 5,
-				Title = "Midnight Secrets",
-				ISBN = 978 - 012345678,
-				Genre = Domain.Enums.Genre.Mystery,
-				AuthorId = 4
+			      Id = 5,
+			      Title = "Midnight Secrets",
+			      ISBN = 978 - 012345678,
+			      Genre = Domain.Enums.Genre.Mystery,
+			      AuthorId = 4
 			});
 
 			modelBuilder.Entity<Customer>().HasData(new Customer
 			{
-				Id = 1,
-				FirstName = "Alice",
-				LastName = "Johnson",
-				Address = "123 Main St, Anytown, USA"
+			      Id = 1,
+			      FirstName = "Alice",
+			      LastName = "Johnson",
+			      Address = "123 Main St, Anytown, USA"
 			});
 
 			modelBuilder.Entity<Customer>().HasData(new Customer
 			{
-				Id = 2,
-				FirstName = "Alice",
-				LastName = "Johnson",
-				Address = "123 Main St, Anytown, USA"
+			       Id = 2,
+			       FirstName = "Alice",
+			       LastName = "Johnson",
+			       Address = "123 Main St, Anytown, USA"
 			});
 
 			modelBuilder.Entity<Customer>().HasData(new Customer
 			{
-				Id = 3,
-				FirstName = "Carol",
-				LastName = "Davis",
-				Address = "789 Oak St, Smallville"
+			       Id = 3,
+			       FirstName = "Carol",
+			       LastName = "Davis",
+			       Address = "789 Oak St, Smallville"
 			});
 
 			modelBuilder.Entity<Customer>().HasData(new Customer
@@ -245,5 +245,5 @@ namespace BookStoreApp.DataAccess
 
 		}
 
-	}
+      }
 }
