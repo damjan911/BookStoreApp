5520463b9265a144b626e5b3d1ab90c414ccfa83
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 10:46:58 +0200

    Code Editing BookCustomer.cs

--- BookStoreApp.Domain/Models/BookCustomer.cs
+++ BookStoreApp.Domain/Models/BookCustomer.cs
@@ -9,19 +9,20 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.Domain.Models
 {
-	public class BookCustomer 
-	{
-		[Key]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public int Id { get; set; }
-		public Customer? Customer { get; set; }
+    public class BookCustomer 
+    {
+	 [Key]
+	 [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	 public int Id { get; set; }
+	    
+	 public Customer? Customer { get; set; }
 
-		public int CustomerId { get; set; }
+	 public int CustomerId { get; set; }
 
-		public Book? Book { get; set; }
+	 public Book? Book { get; set; }
 
-		public int BookId { get; set; }
+	 public int BookId { get; set; }
 
-		public Genre Genre { get; set; }
-	}
+	 public Genre Genre { get; set; }
+    }
 }
