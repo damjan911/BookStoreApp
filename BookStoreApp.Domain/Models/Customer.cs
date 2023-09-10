aa6eaf36096186b245863986fdecde4ef81bcdc8
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 10:48:50 +0200

    Code Editing Customer.cs

--- BookStoreApp.Domain/Models/Customer.cs
+++ BookStoreApp.Domain/Models/Customer.cs
@@ -8,19 +8,20 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.Domain.Models
 {
-	public class Customer 
-	{
-		[Key]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public int Id { get; set; }
-		[Required]
-		public string FirstName { get; set; } = string.Empty;
+   public class Customer 
+   {
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public int Id { get; set; }
+	   
+	[Required]
+	public string FirstName { get; set; } = string.Empty;
 
-		[Required]
-		public string LastName { get; set; } = string.Empty;
+	[Required]
+	public string LastName { get; set; } = string.Empty;
 
-		public string? Address { get; set; }
+	public string? Address { get; set; }
 
-		public List<BookCustomer> BookCustomers = new List<BookCustomer>();
-	}
+	public List<BookCustomer> BookCustomers = new List<BookCustomer>();
+   }
 }
