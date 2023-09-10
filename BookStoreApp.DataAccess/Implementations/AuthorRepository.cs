2200588daf88b909c349fd4c77a1815eadf1cdd4
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 11:01:03 +0200

    Code Editing AuthorRepository.cs

--- BookStoreApp.DataAccess/Implementations/AuthorRepository.cs
+++ BookStoreApp.DataAccess/Implementations/AuthorRepository.cs
@@ -9,42 +9,42 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.DataAccess.Implementations
 {
-	public class AuthorRepository : IAuthorRepository
-	{
-		private readonly BookStoreAppDbContext _dbContext;
+    public class AuthorRepository : IAuthorRepository
+    {
+	private readonly BookStoreAppDbContext _dbContext;
 
         public AuthorRepository(BookStoreAppDbContext dbContext)
         {
-			_dbContext = dbContext;
+	     _dbContext = dbContext;
         }
         public async Task CreateAsync(Author entity)
-		{
-			await _dbContext.Authors.AddAsync(entity);
-			await _dbContext.SaveChangesAsync();
-		}
-
-		public async Task DeleteAsync(int? id)
-		{
-			Author authorDb = new Author();
-
-			_dbContext.Authors.Remove(authorDb);
-			await _dbContext.SaveChangesAsync();
-		}
-
-		public async Task<List<Author>> GetAllAsync()
-		{
-			return await _dbContext.Authors.ToListAsync();
-		}
-
-		public async Task<Author> GetByIdAsync(int? id)
-		{
-			return await _dbContext.Authors.FirstOrDefaultAsync(x=>x.Id == id);
-		}
-
-		public async Task UpdateAsync(Author entity)
-		{
-			_dbContext.Authors.Update(entity);
-			await _dbContext.SaveChangesAsync();
-		}
+	{
+	    await _dbContext.Authors.AddAsync(entity);
+	    await _dbContext.SaveChangesAsync();
 	}
+
+	public async Task DeleteAsync(int? id)
+	{
+	    Author authorDb = new Author();
+
+	    _dbContext.Authors.Remove(authorDb);
+	    await _dbContext.SaveChangesAsync();
+	}
+
+	public async Task<List<Author>> GetAllAsync()
+	{
+	    return await _dbContext.Authors.ToListAsync();
+	}
+
+	public async Task<Author> GetByIdAsync(int? id)
+        {
+	    return await _dbContext.Authors.FirstOrDefaultAsync(x=>x.Id == id);
+	}
+
+	public async Task UpdateAsync(Author entity)
+	{
+	    _dbContext.Authors.Update(entity);
+	    await _dbContext.SaveChangesAsync();
+	}
+     }
 }
