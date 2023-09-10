2e534bf319cd39ce2f6adc7f815c946c030a7513
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 11:06:45 +0200

    Code Editing BookRepository.cs

--- BookStoreApp.DataAccess/Implementations/BookRepository.cs
+++ BookStoreApp.DataAccess/Implementations/BookRepository.cs
@@ -10,74 +10,74 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.DataAccess.Implementations
 {
-	public class BookRepository : IBookRepository
-	{
-		private readonly BookStoreAppDbContext _dbContext;
+    public class BookRepository : IBookRepository
+    {
+	private readonly BookStoreAppDbContext _dbContext;
 
         public BookRepository(BookStoreAppDbContext dbContext)
         {
-			_dbContext = dbContext;
+	     _dbContext = dbContext;
         }
         public async Task CreateAsync(Book entity)
-		{
-			await _dbContext.Books.AddAsync(entity);
-			await _dbContext.SaveChangesAsync();
-		}
+	{
+		await _dbContext.Books.AddAsync(entity);
+		await _dbContext.SaveChangesAsync();
+	}
 
-		public async Task DeleteAsync(int? id)
-		{
-			Book bookDb = new Book();
+	public async Task DeleteAsync(int? id)
+	{
+	     Book bookDb = new Book();
 
-			_dbContext.Remove(bookDb);
-			await _dbContext.SaveChangesAsync();
-		}
+	     _dbContext.Remove(bookDb);
+	     await _dbContext.SaveChangesAsync();
+	}
 
-		public List<Book> FilterBooks(int authorId, int isbn, IEnumerable<BookCustomer> bookCustomers)
-		{
-			if(authorId == 0 && isbn == 0 && bookCustomers == null)
-			{
-				return  _dbContext.Books.ToList();
-			}
+	public List<Book> FilterBooks(int authorId, int isbn, IEnumerable<BookCustomer> bookCustomers)
+	{
+	    if(authorId == 0 && isbn == 0 && bookCustomers == null)
+	    {
+		return  _dbContext.Books.ToList();
+	    }
 
-			if(authorId == 0)
-			{
-				List<Book> bookDb = _dbContext.Books.Where(x=>x.ISBN == isbn && x.BookCustomers == bookCustomers).ToList();
-				return bookDb;
-			}
+	    if(authorId == 0)
+	    {
+		  List<Book> bookDb = _dbContext.Books.Where(x=>x.ISBN == isbn && x.BookCustomers == bookCustomers).ToList();
+		  return bookDb;
+	    }
 
-			if(isbn == 0)
-			{
-				List<Book> bookDb = _dbContext.Books.Where(x=>x.AuthorId == authorId && x.BookCustomers == bookCustomers).ToList();
-				return bookDb;
-			}
+	    if(isbn == 0)
+	    {
+		  List<Book> bookDb = _dbContext.Books.Where(x=>x.AuthorId == authorId && x.BookCustomers == bookCustomers).ToList();
+		  return bookDb;
+	    }
 
-			if(bookCustomers == null)
-			{
-				List<Book> bookDb = _dbContext.Books.Where(x => x.AuthorId == authorId && x.ISBN == isbn).ToList();
-				return bookDb;
-			}
+	    if(bookCustomers == null)
+	    {
+		  List<Book> bookDb = _dbContext.Books.Where(x => x.AuthorId == authorId && x.ISBN == isbn).ToList();
+		  return bookDb;
+	    }
 
-			List<Book> books =  _dbContext.Books
-				.Where(x => x.AuthorId == authorId && x.ISBN == isbn && x.BookCustomers == bookCustomers)
-				.ToList();
+	        List<Book> books =  _dbContext.Books
+		                    .Where(x => x.AuthorId == authorId && x.ISBN == isbn && x.BookCustomers == bookCustomers)
+				    .ToList();
 
-			return books;
-		}
+		return books;
+	  }
 
-		public async Task<List<Book>> GetAllAsync()
-		{
-			return await _dbContext.Books.ToListAsync();
-		}
+	  public async Task<List<Book>> GetAllAsync()
+	  {
+	       return await _dbContext.Books.ToListAsync();
+	  }
 
-		public async Task<Book> GetByIdAsync(int? id)
-		{
-			return await _dbContext.Books.FirstOrDefaultAsync(x => x.Id == id);
-		}
+	   public async Task<Book> GetByIdAsync(int? id)
+	  {
+		return await _dbContext.Books.FirstOrDefaultAsync(x => x.Id == id);
+	  }
 
-		public async Task UpdateAsync(Book entity)
-		{
-			_dbContext.Books.Update(entity);
-			await _dbContext.SaveChangesAsync();
-		}
+	  public async Task UpdateAsync(Book entity)
+	  {
+		_dbContext.Books.Update(entity);
+		await _dbContext.SaveChangesAsync();
+	  }
 	}
 }
