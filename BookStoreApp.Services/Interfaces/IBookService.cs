f8ddf524f949ab9103042ba37afa4ba473c94fd2
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 11:53:04 +0200

    Code Editing IBookService.cs

--- BookStoreApp.Services/Interfaces/IBookService.cs
+++ BookStoreApp.Services/Interfaces/IBookService.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.Services.Interfaces
 {
-	public interface IBookService
-	{
-		Task<BookDto> GetBookByIdAsync(int? id);
+    public interface IBookService
+    {
+	 Task<BookDto> GetBookByIdAsync(int? id);
 
-		Task<IEnumerable<BookDto>> GetAllBooksAsync();
+	 Task<IEnumerable<BookDto>> GetAllBooksAsync();
 
-		Task CreateBookAsync (CreateBookDto createBookDto);
+	 Task CreateBookAsync (CreateBookDto createBookDto);
 
-		Task DeleteBookAsync (int? id);
+	 Task DeleteBookAsync (int? id);
 
-		Task UpdateBookAsync (CreateBookDto createBookDto, int? id);
-	}
+	 Task UpdateBookAsync (CreateBookDto createBookDto, int? id);
+    }
 }
