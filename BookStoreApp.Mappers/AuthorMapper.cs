b83cb3d6f386b03d54a2d70669c88d7236597085
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 11:35:04 +0200

    Code Editing AuthorMapper.cs

--- BookStoreApp.Mappers/AuthorMapper.cs
+++ BookStoreApp.Mappers/AuthorMapper.cs
@@ -8,28 +8,28 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.Mappers
 {
-	public static class AuthorMapper
-	{
-		public static AuthorDto MapToAuthorDto (this Author author)
-		{
-			return new AuthorDto()
-			{
-				FirstName = author.FirstName,
-				LastName = author.LastName,
-				City = author.City,
-				State = author.State
-			};
-		}
+    public static class AuthorMapper
+    {
+	  public static AuthorDto MapToAuthorDto (this Author author)
+	  {
+	      return new AuthorDto()
+	      {
+		    FirstName = author.FirstName,
+		    LastName = author.LastName,
+		    City = author.City,
+		    State = author.State
+	       };
+          }
 
-		public static Author MapToAuthor (this CreateAuthorDto createAuthorDto)
+	   public static Author MapToAuthor (this CreateAuthorDto createAuthorDto)
+	   {
+		return new Author()
 		{
-			return new Author()
-			{
-				FirstName = createAuthorDto.FirstName,
-				LastName = createAuthorDto.LastName,
-				City = createAuthorDto.City,
-				State = createAuthorDto.State
-			};
-		}
+		     FirstName = createAuthorDto.FirstName,
+		     LastName = createAuthorDto.LastName,
+		     City = createAuthorDto.City,
+		     State = createAuthorDto.State
+		};
+	    }
 	}
 }
