af509e34e0542aef29053ddec0bf6d20d6f96306
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 11:46:41 +0200

    Code Editing BookService.cs

--- BookStoreApp.Services/Implementations/BookService.cs
+++ BookStoreApp.Services/Implementations/BookService.cs
@@ -12,66 +12,66 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.Services.Implementations
 {
-	public class BookService : IBookService
-	{
-		private readonly IRepository<Book> _bookRepository;
+     public class BookService : IBookService
+     {
+	 private readonly IRepository<Book> _bookRepository;
 
-        public BookService(IRepository<Book> bookRepository)
-        {
+         public BookService(IRepository<Book> bookRepository)
+         {
             _bookRepository = bookRepository;
-        }
-        public async Task CreateBookAsync(CreateBookDto book)
-		{
-			Book bookEntity = book.MapToBook();
+         }
+         public async Task CreateBookAsync(CreateBookDto book)
+	 {
+	      Book bookEntity = book.MapToBook();
 
-			await _bookRepository.CreateAsync(bookEntity);
+	      await _bookRepository.CreateAsync(bookEntity);
+	 }
+
+	  public async Task DeleteBookAsync(int? id)
+	  {
+	       await _bookRepository.DeleteAsync(id);
+	  }
+
+	  public async Task<IEnumerable<BookDto>> GetAllBooksAsync()
+	  {
+		List<Book> books = await _bookRepository.GetAllAsync();
+
+		if(books == null)
+		{
+		     throw new NotImplementedException("Note is null.");
 		}
 
-		public async Task DeleteBookAsync(int? id)
-		{
-			await _bookRepository.DeleteAsync(id);
-		}
+		return books.Select(book=>book.MapToBookDto()).ToList();
+	   }
 
-		public async Task<IEnumerable<BookDto>> GetAllBooksAsync()
-		{
-			List<Book> books = await _bookRepository.GetAllAsync();
+	   public async Task<BookDto> GetBookByIdAsync(int? id)
+	   {
+		Book book = await _bookRepository.GetByIdAsync(id);
 
-			if(books == null)
-			{
-				throw new NotImplementedException("Note is null.");
-			}
+		if (book == null)
+		 {
+		      throw new NotImplementedException("Book is null.");
+		 }
 
-			return books.Select(book=>book.MapToBookDto()).ToList();
-		}
+		 return book.MapToBookDto();
 
-		public async Task<BookDto> GetBookByIdAsync(int? id)
-		{
-			Book book = await _bookRepository.GetByIdAsync(id);
+	    }
 
-			if (book == null)
-			{
-				throw new NotImplementedException("Book is null.");
-			}
+	    public async Task UpdateBookAsync(CreateBookDto createBookDto, int? id)
+	    {
+		  Book bookDb = await _bookRepository.GetByIdAsync(id);
 
-			return book.MapToBookDto();
+	          if (bookDb == null)
+		  {
+			throw new NotImplementedException("Book is null");
+		  }
 
-		}
-
-		public async Task UpdateBookAsync(CreateBookDto createBookDto, int? id)
-		{
-			Book bookDb = await _bookRepository.GetByIdAsync(id);
-
-			if (bookDb == null)
-			{
-				throw new NotImplementedException("Book is null");
-			}
-
-			bookDb.Title = createBookDto.Title;
-			bookDb.ISBN = (int)createBookDto.ISBN;
-			bookDb.Genre = createBookDto.Genre;
-			bookDb.AuthorId = createBookDto.AuthorId;
+		  bookDb.Title = createBookDto.Title;
+		  bookDb.ISBN = (int)createBookDto.ISBN;
+		  bookDb.Genre = createBookDto.Genre;
+		  bookDb.AuthorId = createBookDto.AuthorId;
             
-			await _bookRepository.UpdateAsync(bookDb);
-		}
-	}
+		  await _bookRepository.UpdateAsync(bookDb);
+	     }
+	 }
 }
