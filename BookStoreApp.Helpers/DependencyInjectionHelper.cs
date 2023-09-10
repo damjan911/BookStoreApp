a701f49da65ee734fbd3b85494516847cbcf260e
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 11:27:24 +0200

    Code Editing DependencyInjectionHelper.cs

--- BookStoreApp.Helpers/DependencyInjectionHelper.cs
+++ BookStoreApp.Helpers/DependencyInjectionHelper.cs
@@ -14,25 +14,25 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.Helpers
 {
-	public static class DependencyInjectionHelper
+    public static class DependencyInjectionHelper
+    {
+	 public static void InjectDbContext (this IServiceCollection services)
+	 {
+	      services.AddDbContext<BookStoreAppDbContext> (options => options.UseSqlServer(@"Data Source=(localdb)\MSSQLServer;Database=BookStoreApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+	 }
+
+	 public static void InjectRepositories(this IServiceCollection services)
+	 {
+		services.AddTransient<IRepository<Book>, BookRepository>();
+		services.AddTransient<IRepository<Author>, AuthorRepository>();
+		services.AddTransient<IRepository<Customer>, CustomerRepository>();
+	 }
+
+	public static void InjectServices (this IServiceCollection services)
 	{
-		public static void InjectDbContext (this IServiceCollection services)
-		{
-			services.AddDbContext<BookStoreAppDbContext> (options => options.UseSqlServer(@"Data Source=(localdb)\MSSQLServer;Database=BookStoreApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
-		}
-
-		public static void InjectRepositories(this IServiceCollection services)
-		{
-			services.AddTransient<IRepository<Book>, BookRepository>();
-			services.AddTransient<IRepository<Author>, AuthorRepository>();
-			services.AddTransient<IRepository<Customer>, CustomerRepository>();
-		}
-
-		public static void InjectServices (this IServiceCollection services)
-		{
-			services.AddTransient<IBookService, BookService>();
-			services.AddTransient<IAuthorService, AuthorService>();
-			services.AddTransient<ICustomerService, CustomerService>();
-		}
+		services.AddTransient<IBookService, BookService>();
+		services.AddTransient<IAuthorService, AuthorService>();
+		services.AddTransient<ICustomerService, CustomerService>();
 	}
+     }
 }
