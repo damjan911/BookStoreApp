6acfc4b38d7ddafe83802ae5abce936f22551158
Damjan <119892938+damjan911@users.noreply.github.com>
2023-09-10 11:53:53 +0200

    Code Editing ICustomerService.cs

--- BookStoreApp.Services/Interfaces/ICustomerService.cs
+++ BookStoreApp.Services/Interfaces/ICustomerService.cs
@@ -8,16 +8,16 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.Services.Interfaces
 {
-	public interface ICustomerService 
-	{
-		Task<CustomerDto> GetCustomerByIdAsync(int? id);
+     public interface ICustomerService 
+     {
+	  Task<CustomerDto> GetCustomerByIdAsync(int? id);
 
-		Task<IEnumerable<CustomerDto>> GetAllCustomersAsync();
+	  Task<IEnumerable<CustomerDto>> GetAllCustomersAsync();
 
-		Task CreateCustomerAsync(CreateCustomerDto createCustomerDto);
+	  Task CreateCustomerAsync(CreateCustomerDto createCustomerDto);
 
-		Task DeleteCustomerAsync(int? id);
+	  Task DeleteCustomerAsync(int? id);
 
-		Task UpdateCustomerAsync(CreateCustomerDto createCustomerDto, int? id);
-	}
+	  Task UpdateCustomerAsync(CreateCustomerDto createCustomerDto, int? id);
+     }
 }
